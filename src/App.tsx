6ad34a3b0f8a670b6e57687cce4ccf8dd4096523
Abhishek <abhishek@gkmit.co>
2024-10-03 18:42:48 +0530

    feat: add `router` and `Layout` for router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { AddEvent } from "./components/AddEvent/AddEvent";
+import { EditEvent } from "./components/EditEvent/EditEvent";
 import "@/App.css";
+import { Dashboard } from "./components/Dashboard";
+import { Layout } from "./components/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/event",
+        children: [
+          {
+            path: "/event/add",
+            element: <AddEvent />,
+          },
+          {
+            path: "/event/edit",
+            element: <EditEvent />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <div id="app">
-      <AddEvent />
+      <RouterProvider router={router} />
     </div>
   );
 }
