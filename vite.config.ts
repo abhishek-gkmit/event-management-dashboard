cbf142746c560c97eb039bc441768aa459fddfbd
Abhishek <abhishek@gkmit.co>
2024-10-04 11:17:59 +0530

    fix: remove unnecessary `console.log`

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-console.log(path.resolve(__dirname, "./src/styles/*"));
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
